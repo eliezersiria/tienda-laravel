3d60047982b21ffea0806e93e5c4c333291919ea
Eliezer Siria <eliezer.siria@gmail.com>
2025-08-06 23:44:18 -0600

    Se concluye la fn mandar a la papelera, se estiliza boton de borrar, se mejora la experiencia

--- app/Livewire/EliminarMarcaVehiculo.php
+++ app/Livewire/EliminarMarcaVehiculo.php
@@ -8,22 +8,35 @@ use app\Models\MarcasVehiculo;
 class EliminarMarcaVehiculo extends Component
 {
     public $MarcaId;
-
-    public function eliminar($MarcaId)
+    public $nombre_marca;
+    public $mostrarFormulario = true;
+    public function mount($MarcaId)
     {
-        $marca = MarcasVehiculo::find($MarcaId);
+        // Busca la marca en la base de datos por su ID
+        $marca = MarcasVehiculo::findOrFail($MarcaId);
+
+        // Asigna los valores a las propiedades públicas del componente
+        $this->MarcaId = $marca->id;
+        $this->nombre_marca = $marca->nombre_marca;
+
+    }
+
+    public function eliminar()
+    {
+        $marca = MarcasVehiculo::find($this->MarcaId);
         if ($marca)
         {
-            $marca->delete();
+            sleep(1);
+            //$marca->delete();
 
             // Opcional: emitir mensaje o evento
-            session()->flash('message', 'Post enviado a papelera.');
+            session()->flash('message', 'La marca '.$this->nombre_marca.' fue enviada a papelera.');
+            $this->mostrarFormulario = false; // Oculta el formulario
         }
     }
 
     public function render()
     {
-        
-        return view('marcas.show-marcas', compact('marcas','tiempo','numeroFilas'));
+        return view('livewire.eliminar-marca-vehiculo');
     }
 }
